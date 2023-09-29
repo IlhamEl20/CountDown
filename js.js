b32bf01e91a06c2269414e59ab1d4800025ae8ab
Ilham <77104410+IlhamEl20@users.noreply.github.com>
2023-09-29 08:31:53 +0700

    Update 2

--- js.js
+++ js.js
@@ -11,7 +11,7 @@
       mm = String(today.getMonth() + 1).padStart(2, "0"),
       yyyy = today.getFullYear(),
       nextYear = yyyy + 1,
-      dayMonth = "01/09/",
+      dayMonth = "1/9/",
       birthday = dayMonth + yyyy;
   
   today = mm + "/" + dd + "/" + yyyy;
